97c2fa3e654ec7ddde371eeb04ad252e87a928b9
sattarkf <kfsattar@gmail.com>
2021-08-31 14:11:57 +0530

    new code

--- src/main/java/com/sattarkf/springboot/HelloController.java
+++ src/main/java/com/sattarkf/springboot/HelloController.java
@@ -1,18 +1,29 @@
 package com.sattarkf.springboot;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+/*
+ * @Controller public class HelloController {
+ * 
+ * 
+ * @GetMapping({"/", "/hello"}) public String hello(@RequestParam (value =
+ * "name", defaultValue = "World", required = true) String name, Model model) {
+ * model.addAttribute("name", name); return "hello";
+ * 
+ * }
+ * 
+ * 
+ * 
+ * }
+ */
+
+@RestController
 public class HelloController {
-	
-	@GetMapping({"/", "/hello"})
-	public String hello(@RequestParam (value = "name", defaultValue = "World", required = true) String name, Model model) {
-		model.addAttribute("name", name);
-		return "hello";
-		
+ 
+	@RequestMapping("/hello")
+	public String index() {
+		return "Spring Boot Example!!";
 	}
-
+ 
 }
